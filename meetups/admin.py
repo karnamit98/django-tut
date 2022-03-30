bbf1d4adf39670526c551b5d61099de6c6606300
Amit Karn <amit.karn98@gmail.com>
2022-03-30 13:27:31 +0545

    project upload

---
+++ meetups/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from .models import Meetup
+from .models import Location
+from .models import Participant
+
+# Register your models here.
+
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'location')
+    list_filter = ( 'location', 'date', )
+    prepopulated_fields = {'slug': ('title',)}
+    
+
+
+
+admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Location)
+admin.site.register(Participant)
